5eaa7a6b8259f0fb9d3059a153bb21cddc40a515
Rhagavi Thiyagarajan <103434130+rhagavi3@users.noreply.github.com>
2022-05-06 09:28:15 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,11 @@
+function foo()
+{
+    var first=document.getElementById("firstname").value;
+console.log(first)
+var mid=document.getElementById("middlename").value;
+console.log(mid)
+var last=document.getElementById("lastname").value
+console.log(last)
+var submit=document.getElementById("submit").value;
+console.log(submit)
+}
